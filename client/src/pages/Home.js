60aa84d85a87b63293d7f00642e7e6f142025ccd
Prakhar Saxena <prakharsxn7@gmail.com>
2024-12-03 12:49:07 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = function () {
             <div className="container mx-auto px-8 lg:flex">
               <div className="text-center lg:text-left lg:w-1/2">
                 <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-                  CryptoTracker
+                  CryptoSpectra
                 </h1>
                 <p className="text-xl lg:text-2xl mt-6 font-light">
                 Get real-time cryptocurrency updates, track your favorites, and stay ahead with comprehensive market insights—all in one place.
